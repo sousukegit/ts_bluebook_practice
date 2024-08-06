94c6dd4e0012529140c81f4bf95866901df54b9d
sousukegit <greenneosoul.sousuke@gmail.com>
2024-08-06 17:12:08 +0900

    init

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,6 @@ type HasName = {
     name: string;
 };
 
-
-
 type HasNameFamily<Parent extends HasName,Child extends HasName> = {
     mother:Parent;
     father:Parent;
@@ -43,4 +41,75 @@ const arr3: readonly number[] = [1,10,100]
 
 for(const num of arr3){
     console.log(num)
+}
+
+type Drink = {
+    label:string;
+    price?:number;
+    kinds:string;
+    shape:{
+        capShape:string;
+        bottleShape:string;
+    }
+}
+
+const JapaneseTee:Drink = {
+    label:"緑茶",
+    price:100,
+    kinds:"tee",
+    shape:{
+        capShape:"circle",
+        bottleShape:"500ml"
+    }
+} 
+
+const {label:labelname = "ayataka", price = "600"} = JapaneseTee
+const {kinds = "milk",shape:{capShape} } = JapaneseTee
+
+const {label,...restObj} = JapaneseTee
+
+type User = {
+    name:string;
+    age:number;
+    premiumUser:boolean;
+}
+
+const data: string = `
+uhyo,26,1
+John Smith,17,0
+Mary Sue,14,1
+`;
+
+//改行コードで配列に分ける
+const lines:Array<string> = data.split("/n")
+//ユーザー型の配列を作る
+const Users:Array<User> = []
+
+//
+for(const userInfo of lines){
+    if(userInfo === ""){
+        continue;
+    }
+    
+    //配列の分割代入を行う
+    const [name,ageString,premiumUserString] = userInfo.split(",");
+    //splitの戻り値は文字型なので、指定の方に戻す
+    const age = Number(ageString);
+    const premiumUser = premiumUserString === "1"
+
+    Users.push({name,age,premiumUser})
+    
+    //私のくそコード
+    // const userInfoArray = userInfo.split(",");
+    // if(userInfoArray[2] === 1){
+
+    // }
+
+    // let user:User = {
+    //     name:userInfoArray[0],
+    //     age:userInfoArray[1],
+    //     premiumUser:userInfoArray[2],
+    // }
+
+    
 }
