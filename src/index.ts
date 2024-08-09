4d5af363d306075942940eeac5e6e7ba16d81dcf
sousukegit <greenneosoul.sousuke@gmail.com>
2024-08-09 21:08:31 +0900

    練習

--- src/index.ts
+++ src/index.ts
@@ -68,51 +68,6 @@ const {kinds = "milk",shape:{capShape} } = JapaneseTee
 
 const {label,...restObj} = JapaneseTee
 
-type User = {
-    name:string;
-    age:number;
-    premiumUser:boolean;
-}
-
-const data: string = `
-uhyo,26,1
-John Smith,17,0
-Mary Sue,14,1
-`;
-
-//改行コードで配列に分ける
-const lines:Array<string> = data.split("/n")
-//ユーザー型の配列を作る
-const Users:Array<User> = []
-
-//
-for(const userInfo of lines){
-    if(userInfo === ""){
-        continue;
-    }
-    
-    //配列の分割代入を行う
-    const [name,ageString,premiumUserString] = userInfo.split(",");
-    //splitの戻り値は文字型なので、指定の方に戻す
-    const age = Number(ageString);
-    const premiumUser = premiumUserString === "1"
-
-    Users.push({name,age,premiumUser})
-    
-    //私のくそコード
-    // const userInfoArray = userInfo.split(",");
-    // if(userInfoArray[2] === 1){
-
-    // }
-
-    // let user:User = {
-    //     name:userInfoArray[0],
-    //     age:userInfoArray[1],
-    //     premiumUser:userInfoArray[2],
-    // }
-
-    
-}
 function range(min:number,max:number):number[]{
     const result = []
     for(let i = min; i < max; i++){
@@ -185,7 +140,7 @@ const nandemoArray_o = {
 const pair = <Left,Right>(left:Left,right:Right):[Left,Right] => {
     return [left,right]
 }
-const p = pair<string,number>("manabe",30)
+const p = pair("manabe",30)
 
 const nandemoArray_ex = <T extends {name:string}>(element:T,length:number):T[] => {
     const result:T[] = []
@@ -200,7 +155,10 @@ type Pet = {
     age:number;
 };
 
-console.log(nandemoArray_ex<Pet>({
+console.log(nandemoArray_ex({
     name:"udon",
     age:4
 },3))
+
+
+
---
+++ src/practice.ts
@@ -0,0 +1,79 @@
+
+
+type User = {
+    name:string;
+    age:number;
+    premiumUser:boolean;
+}
+
+const data: string = `
+uhyo,26,1
+John Smith,17,0
+Mary Sue,14,1
+`;
+
+//改行コードで配列に分ける
+const lines:Array<string> = data.split("/n")
+//ユーザー型の配列を作る
+const Users:Array<User> = []
+
+//
+for(const userInfo of lines){
+    if(userInfo === ""){
+        continue;
+    }
+    
+    //配列の分割代入を行う
+    const [name,ageString,premiumUserString] = userInfo.split(",");
+    //splitの戻り値は文字型なので、指定の方に戻す
+    const age = Number(ageString);
+    const premiumUser = premiumUserString === "1"
+
+    Users.push({name,age,premiumUser})
+    
+    //私のくそコード
+    // const userInfoArray = userInfo.split(",");
+    // if(userInfoArray[2] === 1){
+
+    // }
+
+    // let user:User = {
+    //     name:userInfoArray[0],
+    //     age:userInfoArray[1],
+    //     premiumUser:userInfoArray[2],
+    // }
+
+    
+}
+
+
+
+
+const getFizzBuzzString = (num:number):string => {
+
+    if(num % 3 === 0 && num % 5 === 0){
+        return "FizzBuzz"
+    }
+    else if(num % 3 === 0 ) {
+        return "Fizz"
+    }
+    else if(num % 5 === 0){
+        return "Buzz"
+    }
+    else{
+        return String(num)
+    }
+}
+
+const sequence = (min:number,max:number):number[] => {
+    const numarray:number[] = [];
+    for(let i = min;i<=max;i++){
+        numarray.push(i);
+    }
+    return numarray;
+}
+
+for(const i of sequence(1,100)){
+    const message = getFizzBuzzString(i);
+    console.log(message)
+}
