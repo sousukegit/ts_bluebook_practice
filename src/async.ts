3dc1b3a97d5db2c735d3f3a1a82308b91d86d499
sousukegit <greenneosoul.sousuke@gmail.com>
2024-08-25 08:56:54 +0900

    Promise版のFSでPromiseの便利さを確認する

--- src/async.ts
+++ src/async.ts
@@ -1,15 +1,16 @@
 import {readFile} from "fs";
 import { performance } from "perf_hooks";
+import * as fs_p from "fs/promises";
 
 //非同期処理
 //ファイルを読み込むという非同期処理が終了したら、コールバック関数実行
 const startTime = performance.now()
 const data = readFile("practice.txt","utf-8",(err,data)=>{
     const endTime = performance.now()
-    //console.log(`${endTime-startTime}ミリ秒かかりました`)
+    console.log(`${endTime-startTime}ミリ秒かかりました`)
 })
 
-//console.log(data)
+console.log(data)
 
 //非同期処理に渡した関数は同期処理が終わるまで実行されない
 setTimeout(()=>{console.log("タイマーが呼び出されました")},100);
@@ -20,3 +21,9 @@ while(performance.now()-startTime_2<1000){
     count ++;
 }
 console.log(count);
+
+//promise
+//promise版のFSを使う
+const p = fs_p.readFile("practice.txt","utf-8")
+//非同期処理が終わった後、then以降の関数が実行される
+p.then((data)=>{console.log(data)})
